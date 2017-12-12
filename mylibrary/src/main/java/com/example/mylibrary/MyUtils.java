fa18856f2cfbee40f579520866e689e6d30d612a
myml666 <18954151895@163.com>
2017-12-12 11:10:11 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/MyUtils.java
@@ -0,0 +1,12 @@
+package com.example.mylibrary;
+
+/**
+ * Created by Administrator on 2017/12/12 0012.
+ * Date 2017/12/12 0012
+ */
+
+public class MyUtils {
+    public static int add(int a,int b){
+        return a+b;
+    }
+}
